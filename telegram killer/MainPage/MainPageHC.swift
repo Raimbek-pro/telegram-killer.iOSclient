d48c194f76c8c00d414cb7d37b45e6130f79134f
Raiymbek <76900558+Raimbek-pro@users.noreply.github.com>
2026-03-19 20:16:46 +0500

    swiftdata added for local saving of chats from  server changes

---
+++ telegram killer/MainPage/MainPageHC.swift
@@ -0,0 +1,7 @@
+//
+//  MainPageVC.swift
+//  telegram killer
+//
+//  Created by Райымбек Омаров on 19.03.2026.
+//
+
---
+++ telegram killer/managers/SwiftDataContextManager.swift
@@ -0,0 +1,7 @@
+//
+//  SwiftDataContextManager.swift
+//  telegram killer
+//
+//  Created by Райымбек Омаров on 18.03.2026.
+//
+
